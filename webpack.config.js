fbbc65c839e9334e2d94c13028a22de09f50b306
Roman Dubinin <romanonthego@gmail.com>
2015-10-10 14:50:43 +0600

    reinit oops

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var _ = require('lodash');
+var webpack = require('webpack');
+var banner = require('./utils/banner');
+var LIB_NAME = 'react-video';
+var GLOBAL_VAR = 'ReactVideo';
+
+module.exports = {
+  entry: './lib/' + LIB_NAME + '.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: LIB_NAME + '.js',
+    libraryTarget: 'umd',
+    library: GLOBAL_VAR
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react'
+    }
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+      }
+    }),
+    new webpack.BannerPlugin(banner, { raw: true, entryOnly: true })
+  ]
+};
